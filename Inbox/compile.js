48935696cde2f4651b233ad3e2c7c781d71557a7
himanshu mandwal <hkushwah8@gmail.com>
2021-09-22 22:40:39 +0530

    added the test script for our first contract

--- Inbox/compile.js
+++ Inbox/compile.js
@@ -10,10 +10,10 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname,'contracts','index.sol');
-const source  = fs.readFileSync(inboxPath,'utf-8');
+const inboxPath = path.resolve(__dirname, 'contracts', 'index.sol');
+const source = fs.readFileSync(inboxPath, 'utf-8');
 
-module.exports =  solc.compile(source, 1).contracts[':Inbox']; // exporting the object
+module.exports = solc.compile(source, 1).contracts[':Inbox']; // exporting the object
 /*
 {
   contracts :{
